773a03bf7a8e88b1cd3f4c9e7227c6ea8c952c9e
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-01-17 12:24:58 +0100

    Update images and styles in Feed component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,19 +4,21 @@ import electronics from "@/public/assets/images/electronics.jpg";
 import exercise from "@/public/assets/images/exercise.jpg";
 import kids from "@/public/assets/images/kids.jpeg";
 import shoes from "@/public/assets/images/shoes.jpg";
-import watches from "@/public/assets/images/watches.jpg";
+import lazy from "@/public/assets/images/lazy.png";
 import workoutClothes from "@/public/assets/images/workout-clothes.jpg";
+
 import Image from "next/image";
 
 const Feed = () => {
   return (
     <div className="w-full">
-      <div className="grid grid-cols-5 grid-rows-4 gap-3 h-[80vh] [&>*]:rounded-2xl [&>*]:overflow-hidden [&>*]:relative [&>*]:after:content-none [&>*]:after:absolute [&>*]:after:left-0 [&>*]:after:top-0 [&>*]:after:w-full [&>*]:after:h-full [&>*]:after:opacity-0 [&>*]:after:transition-all [&>*]:cursor-pointer">
-        <div className="image-con item-1">
+      <div className="grid grid-cols-5 grid-rows-4 gap-3 h-[80vh] [&>*]:rounded-3xl [&>*]:overflow-hidden [&>*]:relative [&>*]:after:content-none [&>*]:after:absolute [&>*]:after:left-0 [&>*]:after:top-0 [&>*]:after:w-full [&>*]:after:h-full [&>*]:after:opacity-0 [&>*]:after:transition-all [&>*]:cursor-pointer [&>*]:shadow-md">
+        <div className="image-con col-span-2">
           <Image
             src={decor}
             alt="decor"
             quality={40}
+            placeholder="blur"
             fill
             style={{
               objectFit: "cover",
@@ -28,41 +30,29 @@ const Feed = () => {
             Home Decor
           </p>
         </div>
-        <div className="image-con item-2">
+        <div className="image-con col-span-2">
           <Image
-            src={shoes}
-            alt="shoes"
+            src={workoutClothes}
+            alt="workoutClothes"
             quality={40}
+            placeholder="blur"
             fill
             style={{
               objectFit: "cover",
               transition: "all 0.5s ease all",
-            }}
-          />
-          <p className="text-white inline-block pb-[2px] bg-right-bottom bg-[0% 2px] bg-no-repeat un z-10 absolute md:left-5 left-2 bottom-3 font-bold text-3xl [writing-mode:tb-rl] -rotate-180 md:rotate-0 md:[writing-mode:horizontal-tb]">
-            Shoes
-          </p>
-        </div>
-        <div className="image-con item-3">
-          <Image
-            src={electronics}
-            alt="electronics"
-            quality={40}
-            fill
-            style={{
-              objectFit: "cover",
-              transition: "all 0.5s ease all",
+              objectPosition: "100% 10%",
             }}
           />
           <p className="text-white inline-block pb-[2px] bg-right-bottom bg-[0% 2px] bg-no-repeat un z-10 absolute left-5 bottom-3 font-bold text-3xl">
-            Electronics
+            Workout clothes
           </p>
         </div>
-        <div className="image-con item-4">
+        <div className="image-con row-span-3">
           <Image
             src={kids}
             alt="kids"
             quality={40}
+            placeholder="blur"
             fill
             style={{
               objectFit: "cover",
@@ -73,11 +63,44 @@ const Feed = () => {
             Kids
           </p>
         </div>
-        <div className="image-con item-5">
+        <div className="image-con row-span-3">
+          <Image
+            src={shoes}
+            alt="shoes"
+            quality={40}
+            placeholder="blur"
+            fill
+            style={{
+              objectFit: "cover",
+              transition: "all 0.5s ease all",
+            }}
+          />
+          <p className="text-white inline-block pb-[2px] bg-right-bottom bg-[0% 2px] bg-no-repeat un z-10 absolute md:left-5 left-2 bottom-3 font-bold text-3xl [writing-mode:tb-rl] -rotate-180 md:rotate-0 md:[writing-mode:horizontal-tb]">
+            Shoes
+          </p>
+        </div>
+        <div className="image-con ">
+          <Image
+            src={electronics}
+            alt="electronics"
+            quality={40}
+            placeholder="blur"
+            fill
+            style={{
+              objectFit: "cover",
+              transition: "all 0.5s ease all",
+            }}
+          />
+          <p className="text-white inline-block pb-[2px] bg-right-bottom bg-[0% 2px] bg-no-repeat un z-10 absolute left-5 bottom-3 font-bold text-3xl">
+            Electronics
+          </p>
+        </div>
+        <div className="image-con ">
           <Image
             src={exercise}
             alt="exercise"
             quality={40}
+            placeholder="blur"
             fill
             style={{
               objectFit: "cover",
@@ -87,22 +110,6 @@ const Feed = () => {
           />
           <p className="text-white inline-block pb-[2px] bg-right-bottom bg-[0% 2px] bg-no-repeat un z-10 absolute left-5 bottom-3 font-bold text-3xl">
             Exercise and Fitness
-          </p>
-        </div>
-        <div className="image-con item-6">
-          <Image
-            src={workoutClothes}
-            alt="workoutClothes"
-            quality={40}
-            fill
-            style={{
-              objectFit: "cover",
-              transition: "all 0.5s ease all",
-              objectPosition: "100% 10%",
-            }}
-          />
-          <p className="text-white inline-block pb-[2px] bg-right-bottom bg-[0% 2px] bg-no-repeat un z-10 absolute left-5 bottom-3 font-bold text-3xl">
-            Workout clothes
           </p>
         </div>
       </div>
