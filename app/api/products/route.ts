2e9ab60c70aac455608aca875c9ee4a0ac029abb
Joshua Aideloje <aidelojejoshua@gmail.com>
2024-01-22 11:21:46 +0100

    📦 NEW: Add GET route for fetching prompts

---
+++ app/api/products/route.ts
@@ -0,0 +1,24 @@
+import { connectToDB } from "@/utils/database";
+import Prompt from "@/models/products";
+
+export const GET = async (req, res) => {
+  try {
+    await connectToDB();
+
+    const prompts = await Prompt.find().populate("createdBy", "image username");
+
+    return new Response(JSON.stringify(prompts), {
+      status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ msg: error.message }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+};
--- models/products.js
+++ models/products.js
@@ -1,3 +1,24 @@
-import {Schema, model, models} from 'mongoose';
+import { Schema, model, models } from "mongoose";
 
-const productSchema = new Schema({})
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+});
